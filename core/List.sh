46b10f3c80e6736757b2a97065333f5e6a1805c3
WLS <77196153+WLS-SD@users.noreply.github.com>
2021-09-19 09:47:42 +0000

    Add files via upload

---
+++ core/List.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+
+
+
+
+                                                                        
+
+GREEN="\e[93m"
+
+printf "$GREEN"
+
+echo "
+|========================================================|
+|DEVELOPER : WLS-SD                                      |   
+|GitHub : github.com/WLS-SD                              | 
+|========================================================|
+| [01] Facebook      [11] Twitch       [21] DeviantArt   |
+| [02] Instagram     [12] Pinterest    [22] Badoo        |
+| [03] Google        [13] Snapchat     [23] Origin       |
+| [04] Microsoft     [14] Linkedin     [24] DropBox      |
+| [05] Netflix       [15] Ebay         [25] Yahoo        |
+| [06] Paypal        [16] Quora        [26] Wordpress    |
+| [07] Steam         [17] Protonmail   [27] Yandex       |
+| [08] Twitter       [18] Spotify      [28] StackoverFlow|
+| [09] Playstation   [19] Reddit       [29] Vk           |
+| [10] Tiktok        [20] Adobe        [30] XBO          |
+| [31] Mediafire     [32] Gitlab       [33] Github       |
+| [99] About         [00] Exit                           |
+|========================================================|
+
+
+"
+
+
+                                                              
