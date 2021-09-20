b311a7a0e22a8622b1e48ebc6fd04e67ab0e1d5c
WLS <77196153+WLS-SD@users.noreply.github.com>
2021-09-20 14:34:20 +0545

    Update show-credentials.sh

--- utilities/show-credentials.sh
+++ utilities/show-credentials.sh
@@ -7,7 +7,20 @@ cd ui
 chmod +x *
 ./stop-fish-u-ui.sh
 
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
 
+echo " [+][+][+] UserNames & Passwords [+][+][+]"
+
+
+
+
+
+
+#usrname_passwd colour
+
+Green="\e[92m"
+printf "${Green}"
 
 #cat username & password
 cd
